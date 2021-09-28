feb80a72e03f11fec4d379f48764981ec63b71ac
Salavat-VR <shashkevich41@gmail.com>
2021-09-28 15:27:09 +0300

    some basic settings stuff

--- src/src/settings.py
+++ src/src/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'src',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -74,12 +76,24 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'src',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '1234',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
